5e7fdbd4af6b10c64115a304625c341422e2cc4a
59013-21-08 <59013-21-08@exchange.ad.afpanet>
2023-12-13 15:26:14 +0100

    first commit dbmanager

---
+++ C/test.action.php
@@ -0,0 +1,49 @@
+<?php
+include_once "../M/DB_Manager.class.php";
+
+/*récupération du tableau user de la base de donnée
+$listUsers = readUsers();
+print_r($listUsers);
+*/  
+
+
+//création d'un nouvel utilisateur à insérer dans la base de donnée
+/*$nameUser = "Scott";
+$firstnameUser = "Michael";
+$telUser = "bonjour";
+$mailUser = "mvbuz@gmail.com";
+$passwordUser = "password1";
+*/
+
+//vérification des conditions avant de créer: name et firstname composés uniquement de lettres, tel composé uniquement de chiffres. 
+/*if(isAlpha($nameUser) && isAlpha($firstnameUser) && isBeta($telUser)) {
+   createUser($nameUser, $firstnameUser, $telUser, $mailUser, $passwordUser);
+  } else {
+	echo "ERROR";
+  }
+*/
+
+/*
+$idUser = 5;
+deleteUser($idUser);
+*/
+
+/*Create new penality
+$libellePenality = "Petite Insulte";
+$pricePenality = 0.10;
+createPenality($libellePenality, $pricePenality);
+*/
+
+/* read penality table
+$listPenalities = readPenalities();
+print_r($listPenalities);
+*/
+
+
+/*delete line in penality table from id
+$idPenality = 1;
+deletePenality($idPenality)
+*/
+
+
+?>
---
+++ M/DB_Manager.class.php
@@ -0,0 +1,129 @@
+<?php
+//@author Mathilde <mathilde..brx@gmail.com>
+
+////////////////////////////////////////////////USER/////////////////////////////////////////////////////////////////////
+//Methode qui renvoie la liste des users
+ function readUsers() : array {
+    //driver vers la DB
+    $bdd = new PDO('mysql:host=localhost;dbname=Game_of_fraudes;charset=utf8mb4', 'root', '');
+    $stmt = $bdd->prepare("SELECT * FROM `user`; ");
+    $stmt->execute();
+    //rapatrie toutes les lignes de la table
+    $listUsers = $stmt->fetchAll();
+    return $listUsers;
+}
+
+
+//methode qui ajoute une personne dans la DB
+function createUser($nameUser, $firstnameUser, $telUser, $mailUser, $passwordUser) : void {       
+    $bdd = new PDO('mysql:host=localhost;dbname=Game_of_fraudes;charset=utf8mb4', 'root', '');
+    $sql = "INSERT INTO user (NAME_USER, FIRSTNAME_USER, TEL, MAIL, PASSWORD) VALUES (?,?,?,?,?)";
+    $stmt= $bdd->prepare($sql);
+    $stmt->execute([$nameUser, $firstnameUser, $telUser, $mailUser, $passwordUser]);
+    }
+
+//fonction qui vérifie que l'entrée ne contient que des lettres
+function isAlpha ($str) {
+ //>((string)str)-(bool)>
+return preg_match('/^([a-zA-Zéèï]*)$/',$str);
+}
+//fonction qui vérifie que l'entrée ne contient que des chiffres
+function isBeta ($str1) {
+    //>((string)str)-(bool)>
+   return preg_match('/^([0-9]*)$/',$str1);
+   }
+
+
+//fonction qui supprime une ligne de la DB
+function deleteUser($idUser): void {
+$bdd = new PDO('mysql:host=localhost;dbname=Game_of_fraudes;charset=utf8mb4', 'root', '');
+$sql = "DELETE FROM user WHERE ID_USER = ? ";
+$stmt= $bdd->prepare($sql);
+$stmt->execute([$idUser]);
+}
+
+ /*  // fonction qui modifie une donnée de la DB
+function updateUser($nameUser, $firstnameUser, $telUser, $mailUser, $passwordUser, $idUser): void {
+$bdd = new PDO('mysql:host=localhost;dbname=Game_of_fraudes;charset=utf8mb4', 'root', '');
+$sql = "UPDATE user SET FIRSTNAME_USER = ? WHERE ID_USER = ?";
+$stmt= $bdd->prepare($sql);
+$stmt->execute([$nameUser, $firstnameUser, $telUser, $mailUser, $passwordUser, $idUser]);
+}
+*/
+
+////////////////////////////////////////////////PENALITY/////////////////////////////////////////////////////////////////////
+//Methode qui renvoie la liste des penalities
+ function readPenalities() : array {
+    //driver vers la DB
+    $bdd = new PDO('mysql:host=localhost;dbname=Game_of_fraudes;charset=utf8mb4', 'root', '');
+    $stmt = $bdd->prepare("SELECT * FROM `penality`; ");
+    $stmt->execute();
+    //rapatrie toutes les lignes de la table
+    $listPenalities = $stmt->fetchAll();
+    return $listPenalities;
+}
+
+
+//methode qui ajoute une personne dans la DB
+function createPenality($libellePenality, $pricePenality) : void {       
+    $bdd = new PDO('mysql:host=localhost;dbname=Game_of_fraudes;charset=utf8mb4', 'root', '');
+    $sql = "INSERT INTO penality (LIBELLE, PRICE) VALUES (?,?)";
+    $stmt= $bdd->prepare($sql);
+    $stmt->execute([$libellePenality, $pricePenality]);
+    }
+
+
+//fonction qui supprime une ligne de la DB
+function deletePenality($idPenality): void {
+    $bdd = new PDO('mysql:host=localhost;dbname=Game_of_fraudes;charset=utf8mb4', 'root', '');
+    $sql = "DELETE FROM penality WHERE ID_PENALITY = ? ";
+    $stmt= $bdd->prepare($sql);
+    $stmt->execute([$idPenality]);
+    }
+
+
+////////////////////////////////////////////////DEBT/////////////////////////////////////////////////////////////////////    
+
+//Methode qui renvoie la liste des debts
+function readDebts() : array {
+    //driver vers la DB
+    $bdd = new PDO('mysql:host=localhost;dbname=Game_of_fraudes;charset=utf8mb4', 'root', '');
+    $stmt = $bdd->prepare("SELECT * FROM `debt`; ");
+    $stmt->execute();
+    //rapatrie toutes les lignes de la table
+    $listDebts = $stmt->fetchAll();
+    return $listDebts;
+}
+
+
+//methode qui ajoute une personne dans la DB
+function createDebt($firstnameUser, $libellePenality, $details) : void {       
+    $bdd = new PDO('mysql:host=localhost;dbname=Game_of_fraudes;charset=utf8mb4', 'root', '');
+    $sql = "INSERT INTO debt (ID_USER, LIBELLE, DETAIL) VALUES (?,?,?)";
+    $stmt= $bdd->prepare($sql);
+    $stmt->execute([$firstnameUser, $libellePenality, $details]);
+    }
+
+
+//fonction qui supprime une ligne de la DB
+function deleteDebt($nbDebt): void {
+    $bdd = new PDO('mysql:host=localhost;dbname=Game_of_fraudes;charset=utf8mb4', 'root', '');
+    $sql = "DELETE FROM debt WHERE NB_DEBT = ? ";
+    $stmt= $bdd->prepare($sql);
+    $stmt->execute([$nbDebt]);
+    }
+
+
+
+    //function qui filtre les dettes par id utilisateur
+    function filtrerDebt() : array {
+        //driver vers la DB
+        $bdd = new PDO('mysql:host=localhost;dbname=Game_of_fraudes;charset=utf8mb4', 'root', '');
+        $stmt = $bdd->prepare("SELECT * FROM `debt` WHERE ID_USER = ? ; ");
+        $stmt->execute();
+        //rapatrie toutes les lignes de la table
+        $listDebts = $stmt->fetchAll();
+        return $listDebts;
+    }
+
+?>
